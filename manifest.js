76da7595c058b91cf8124aa0ad5a9f6325249d15
ParrotFriend <zarcojonnebert@gmail.com>
2026-06-23 23:39:42 +0800

    Add files via upload

---
+++ manifest.js
@@ -0,0 +1,23 @@
+{
+  "name"; "Money Tracker",
+  "short_name"; "MoneyTracker",
+  "description"; "Offline Money Tracker",
+  "start_url"; "./",
+  "scope"; "./",
+  "display"; "standalone",
+  "background_color"; "#1a1a1a",
+  "theme_color"; "#10b981",
+  "orientation"; "portrait",
+  "icons" [
+    {
+      "src": "icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
