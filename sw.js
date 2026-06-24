583bbf1e01888299805200393cae573a5d366197
ParrotFriend <zarcojonnebert@gmail.com>
2026-06-24 10:52:03 +0800

    fix menu

--- sw.js
+++ sw.js
@@ -1,26 +1,24 @@
-const CACHE_NAME = "money-tracker-v2";
+const CACHE_NAME = "money-tracker-v3";
 
 self.addEventListener("install", (event) => {
   self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/manifest.json"
-      ]);
+      return cache.addAll(["/", "/index.html", "/manifest.json"]);
     })
   );
 });
 
 self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((res) => {
-      return res || fetch(event.request);
-    })
+    caches.match(event.request).then((res) => res || fetch(event.request))
   );
 });
